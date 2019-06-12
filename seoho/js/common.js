990cb91660a9c53c660833b02327b0d075f1d3aa
kyung <kyungs9955@gmail.com>
2019-06-12 09:48:17 +0900

    seoho 수정

--- seoho/js/common.js
+++ seoho/js/common.js
@@ -12,23 +12,10 @@ jQuery(document).ready(function($) {
 
    // materialize
    document.addEventListener('DOMContentLoaded', function() {
-      var elems = document.querySelectorAll('.sidenav');
-      var elems = document.querySelectorAll('.collapsible');
-      var elems = document.querySelectorAll('.portfolio-modal, .notice-modal');
-      var elems = document.querySelectorAll('.slider');
-      var instances = M.Sidenav.init(elems, options);
-      var instances = M.Collapsible.init(elems, options);
-      var instances = M.Modal.init(elems, options);
-      var instances = M.Slider.init(elems, options);
+      var elems = document.querySelectorAll('.materialboxed');
+      var instances = M.Materialbox.init(elems, options);
    });
-   $('.sidenav').sidenav({
-      edge: 'right'
-   });
-   $('.collapsible').collapsible();
-   $('.portfolio-modal, .notice-modal').modal();
-   $('.slider').slider({
-      height: 530
-   });
+   $('.materialboxed').materialbox();
 
 
    // Menu Fixed
@@ -39,19 +26,5 @@ jQuery(document).ready(function($) {
       } else {
          nav.removeClass("fixed");
       }
-   });
-
-   // Tab
-   var tabMenu = $('.join-tab-menu a, .portfolio-tab-menu a');
-   var tabContent = $('.join-tab-content, .portfolio-tab-content');
-   tabContent.hide();
-   tabContent.first().show();
-   tabMenu.click(function(event) {
-      event.preventDefault();
-      $(this).parent().addClass('on');
-      $(this).parent().siblings().removeClass('on');
-      var tab = $(this).attr('href');
-      tabContent.not(tab).hide();
-      $(tab).show();
    });
 });

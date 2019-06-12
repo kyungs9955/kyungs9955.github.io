99bb62e737ef135b99a54c94b548329cd3e14138
kyung <kyungs9955@gmail.com>
2019-06-12 10:17:01 +0900

    seoho 수정

--- seoho/js/common.js
+++ seoho/js/common.js
@@ -1,30 +1,21 @@
-jQuery(document).ready(function($) {
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.materialboxed');
+    var instances = M.Materialbox.init(elems, options);
+  });
 
-   // wow
-   wow = new WOW({
-   	boxClass:     'wow',      // 기본값
-   	animateClass: 'animated', // 기본값
-   	offset:       0,          // 기본값
-   	mobile:       true,       // 기본값
-   	live:         true        // 기본값
-   })
-   wow.init();
+  // Or with jQuery
 
-   // materialize
-   document.addEventListener('DOMContentLoaded', function() {
-      var elems = document.querySelectorAll('.materialboxed');
-      var instances = M.Materialbox.init(elems, options);
-   });
-   $('.materialboxed').materialbox();
+  $(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
 
 
-   // Menu Fixed
-   var nav = $('header');
-   $(window).scroll(function () {
-      if ($(this).scrollTop() > 180) {
-      	nav.addClass("fixed");
-      } else {
-         nav.removeClass("fixed");
-      }
-   });
-});
+  // wow
+  wow = new WOW({
+     boxClass:     'wow',      // 기본값
+     animateClass: 'animated', // 기본값
+     offset:       0,          // 기본값
+     mobile:       true,       // 기본값
+     live:         true        // 기본값
+  })
+  wow.init();
